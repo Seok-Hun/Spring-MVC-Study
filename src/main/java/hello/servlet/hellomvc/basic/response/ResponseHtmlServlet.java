8220fe26a4dc1771788e088a9ca5915800dc90f9
Kang Seok Hoon <hamer1233@naver.com>
2024-03-06 10:47:18 +0900

    HTTP 응답 데이터-단순 텍스트, HTML 2-11. 서블릿-HTTP 응답 데이터-단순 텍스트,HTML: 응답 데이터에 텍스트로 HTML 코드를 넣어 클라이언트로 보내는 방법에 대해 알아본다.

---
+++ src/main/java/hello/servlet/hellomvc/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.hellomvc.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Content-Type: text/html;charset=utf-8
+        resp.setContentType("text/html");
+        // HTTP 응답으로 HTML을 반환할 때는 text/html로 지정 필요
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println(" <div>안녕?</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+        // HTML을 문자열로 작성하여 응답
+        // 클라이언트는 받은 문자열을 HTML로 바꿔 표시
+    }
+}
