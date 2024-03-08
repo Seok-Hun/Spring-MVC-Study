749b4afdbf1aed1cd5bd93ca0bf211f73a30db8b
Kang Seok Hoon <hamer1233@naver.com>
2024-03-08 19:16:07 +0900

    프로젝트 생성

---
+++ src/test/java/hello/springmvcbasic_function/SpringMvcBasicFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvcbasic_function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBasicFunctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
