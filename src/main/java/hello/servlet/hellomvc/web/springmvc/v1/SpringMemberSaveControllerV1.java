301c2766ec4711182fba95c23ebb27ede2ed55db
Kang Seok Hoon <hamer1233@naver.com>
2024-03-08 11:32:18 +0900

    스프링 MVC - 시작하기 5-4. 스프링 MVC-구조 이해-스프링 MVC-컨트롤러 통합: 스프링 MVC의 어노테이션을 활용해 v3 컨트롤러 코드를 리팩토링 한다.

---
+++ src/main/java/hello/servlet/hellomvc/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,30 @@
+package hello.servlet.hellomvc.web.springmvc.v1;
+
+import hello.servlet.hellomvc.domain.member.Member;
+import hello.servlet.hellomvc.domain.member.MemberRepository;
+import hello.servlet.hellomvc.web.frontcontroller.ModelView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest req, HttpServletResponse resp) {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView modelView = new ModelAndView("save-result");
+        modelView.addObject("memebr",member);
+        return modelView;
+    }
+}
