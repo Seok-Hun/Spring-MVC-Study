2aa6f675497726903a537ae822a5c7e26cf38598
Kang Seok Hoon <hamer1233@naver.com>
2024-03-09 17:37:04 +0900

    HTTP 요청 파라미터 - 쿼리 파라미터, HTML Form 6-6. 스프링 MVC-기본 기능-HTTP 요청 파라미터-쿼리 파라미터, HTML Form: HttpServletRequest로 요청 파라미터를 받았을 때 처리하는 방법에 대해 알아본다.

---
+++ src/main/java/hello/springmvcbasic_function/basic/request/RequestParamController.java
@@ -0,0 +1,26 @@
+package hello.springmvcbasic_function.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    // HttpServlet으로 파라미터 받기
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username={}, age={}",username, age);
+
+        response.getWriter().write("ok");
+    }
+}
