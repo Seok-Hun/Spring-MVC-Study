464b5debfa79adfc829b52d777a86ffb3234502c
Kang Seok Hoon <hamer1233@naver.com>
2024-03-09 18:21:18 +0900

    스프링 MVC - 기본 기능 6-7. 스프링 MVC-기본 기능-HTTP 요청 파라미터-@RequestParam: @RequestParam으로 파라미터를 받아 처리하는 방법에 대해 알아본다.

--- src/main/java/hello/springmvcbasic_function/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvcbasic_function/basic/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -22,5 +25,73 @@ public class RequestParamController {
         log.info("username={}, age={}",username, age);
 
         response.getWriter().write("ok");
+        // ok라는 뷰를 찾는다.
+    }
+
+    // @RequestParam으로 파라미터 받기
+    @ResponseBody
+    // 반환 문자열에 해당하는 뷰를 찾는 것이 아니라 반환 문자열을 응답 메시지에 보낸다.
+    // @RestController와 동일하게 처리
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String param1,
+            // 아래처럼 작성해도 가능하다.
+            // @RequestParam String username
+            // String username
+            @RequestParam("age") int param2
+            // @RequestParam int age
+            // int age
+    ){
+        log.info("username={}, age={}", param1, param2);
+
+        return "ok";
+        // @RestController가 아닌 @Controller 이므로 "ok"만 반환하면 ok라는 뷰를 찾는다.
+    }
+
+    // @RequestParam의 required 속성 추가
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            // required의 default는 true이다.
+            @RequestParam(required = false) Integer age
+            // int는 기본형이라 null이 들어갈 수 없다.
+            // Integer는 객체형이므로 null이 들어갈 수 있다.
+    ){
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    /**
+     * @RequestParam - defaultValue 사용
+     *
+     * 참고: defaultValue는 빈 문자의 경우에도 적용
+     * /request-param-default?username=
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") Integer age
+    ){
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    /**
+     * @RequestParam - Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}",
+                paramMap.get("username"),
+                paramMap.get("age")
+        );
+        return "ok";
     }
 }
