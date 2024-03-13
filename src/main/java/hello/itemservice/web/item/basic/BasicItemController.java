6b2c042f41d4f85ab0a262c9450041a5aedb1710
Kang Seok Hoon <hamer1233@naver.com>
2024-03-13 16:39:34 +0900

    상품 등록 처리 - @ModelAttribute 7-8. 스프링 MVC-웹 페이지 만들기-상품 등록 처리-@ModelAttribute: @ModelAttribute를 활용해 상품 등록 컨트롤러를 만든다.

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,13 +39,87 @@ public class BasicItemController {
     }
 
     /**
-     * 상품 등록 폼 먼트롤러
+     * 상품 등록 폼 컨트롤러
      * 단순히 뷰 템플릿만 호출
      * @return basic/addForm
      */
     @GetMapping("/add")
     public String addForm(){
         return "basic/addForm";
+    }
+
+    /**
+     * 상품 등록 컨트롤러 - @RequestParam
+     * @RequestParam으로 상품 등록
+     * @return basic/item (상품 상세 보기와 같은 뷰 템플릿)
+     */
+    //@PostMapping("/add")
+    public String addItemV1(
+            @RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam Integer quantity,
+            Model model
+    ){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        // key가 item 문자열이고, value가 item 객체
+
+        return "basic/item";
+        // 상품 저장 후 저장된 내용을 보기 위해 상세 보기 뷰 템플릿을 사용
+        // model 데이터 사용
+    }
+
+    /**
+     * 상품 등록 컨트롤러 - @ModelAttribute
+     * @ModelAttribute로 상품 등록
+     * @return basic/item (상품 상세 보기와 같은 뷰 템플릿)
+     */
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){
+
+        // item 파라미터가 생성되는 과정은 아래 과정과 동일하다.
+        // Item item = new Item();
+        // item.setItemName(itemName);
+        // item.setPrice(price);
+        // item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        // @ModelAttribute("item") 코드가 아래 코드와 동일한 역할이다.
+        // model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     * 상품 등록 컨트롤러 - @ModelAttribute 이름 생략
+     * @ModelAttribute의 이름을 생략한다.
+     * @return basic/item (상품 상세 보기와 같은 뷰 템플릿)
+     */
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item){
+
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    /**
+     * 상품 등록 컨트롤러 - @ModelAttribute 전체 생략
+     * @ModelAttribute 전체를 생략한다..
+     * @return basic/item (상품 상세 보기와 같은 뷰 템플릿)
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item){
+
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
