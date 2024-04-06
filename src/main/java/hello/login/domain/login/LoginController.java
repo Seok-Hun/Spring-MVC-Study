7a761797790bd4a0f6d330330354c7362cf3df12
hamer1233@naver.com <hamer1233@naver.com>
2024-04-06 18:54:11 +0900

    쿠키 사용 - 로그인 구현 6-7. 로그인 처리1-쿠키, 세션-로그인 처리하기-쿠키 사용-로그아웃 구현: 쿠키를 삭제하여 로그아웃을 구현한다.

--- src/main/java/hello/login/domain/login/LoginController.java
+++ src/main/java/hello/login/domain/login/LoginController.java
@@ -53,4 +53,16 @@ public class LoginController {
 
         return "redirect:/";
     }
+
+    private void expireCookie(HttpServletResponse response, String cookieName){
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
 }
