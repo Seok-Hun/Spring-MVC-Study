620fe1701229e23d4b5eaa5c84875388c28dd777
Kang Seok Hoon <hamer1233@naver.com>
2024-04-12 10:57:16 +0900

    프로젝트 생성 8-1. 예외 처리와 오류 페이지-프로젝트 생성: 예외 처리와 오류 페이지 실습을 위해 프로젝트를 생성한다.

---
+++ src/test/java/hello/springmvc_exception/SpringMvcExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvc_exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
