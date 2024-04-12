0701e34fb4ae50355589f089c55338caca863fcf
Kang Seok Hoon <hamer1233@naver.com>
2024-04-12 18:31:51 +0900

    서블릿 예외 처리-오류 화면 제공 8-3. 예외 처리와 오류 페이지-서블릿 예외 처리-오류 화면 제공: 서블릿 컨테이너가 제공하는 오류 화면 기능을 사용한다.

--- src/main/java/hello/springmvc_exception/servlet/ErrorPageController.java
+++ src/main/java/hello/springmvc_exception/servlet/ErrorPageController.java
@@ -4,19 +4,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
-@Component
+@Controller
 public class ErrorPageController {
 
     //RequestDispatcher 상수로 정의되어 있음
-    public static final String ERROR_EXCEPTION = "javax.servlet.error.exception";
-    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type";
-    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
-    public static final String ERROR_REQUEST_URI = "javax.servlet.error.request_uri";
-    public static final String ERROR_SERVLET_NAME = "javax.servlet.error.servlet_name";
-    public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code";
+    String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
+    String ERROR_EXCEPTION_TYPE = "jakarta.servlet.error.exception_type";
+    String ERROR_MESSAGE = "jakarta.servlet.error.message";
+    String ERROR_REQUEST_URI = "jakarta.servlet.error.request_uri";
+    String ERROR_SERVLET_NAME = "jakarta.servlet.error.servlet_name";
+    String ERROR_STATUS_CODE = "jakarta.servlet.error.status_code";
 
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
@@ -27,9 +28,9 @@ public class ErrorPageController {
 
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response){
-        log.info("errorPage 500.html");
+        log.info("errorPage 500");
         printErrorInfo(request);
-        return "error-page/500.html";
+        return "error-page/500";
     }
 
     private void printErrorInfo(HttpServletRequest request){
