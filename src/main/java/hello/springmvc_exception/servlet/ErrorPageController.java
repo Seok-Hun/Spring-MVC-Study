2ea4468b6bc22c5b77c5ce9a49ab9d49661cf666
Kang Seok Hoon <hamer1233@naver.com>
2024-04-15 23:38:43 +0900

    스프링 부트 기본 오류 처리 9-2. API 예외 처리-스프링 부트 기본 오류 처리: 스프링 부트의 BasicErrorController를 이용해 API의 예외처리를 수행한다.

--- src/main/java/hello/springmvc_exception/servlet/ErrorPageController.java
+++ src/main/java/hello/springmvc_exception/servlet/ErrorPageController.java
@@ -53,7 +53,7 @@ public class ErrorPageController {
         result.put("message", ex.getMessage());
 
         Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        return new ResponseEntity(result, HttpStatus.valueOf(statusCode));
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
     }
 
     private void printErrorInfo(HttpServletRequest request){
